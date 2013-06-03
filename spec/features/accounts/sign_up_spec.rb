4f50893e2fe2ad512ddf8084d3792dcdfae8fa43
ernane <ernane.sena@gmail.com>
2013-06-02 23:31:19 -0300

    Adicionando cadastro da conta o subdmonio

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -5,8 +5,9 @@ feature "Accounts" do
     visit subscribem.root_url
     click_link "Account Sign Up"
 
-    fill_in "Name",  with: "Jhon Doe"
-    fill_in "Email", with: "jhon@example.com"
+    fill_in "Name",      with: "Jhon Doe"
+    fill_in "Email",     with: "jhon@example.com"
+    fill_in "Subdomain", with: "test"
     password_field_id = "account_owner_attributes_password"
     fill_in password_field_id,       with: "password"
     fill_in "Password confirmation", with: "password"
@@ -16,5 +17,6 @@ feature "Accounts" do
     success_message = "Your account has been successfully created."
     page.should have_content(success_message)
     page.should have_content("jhon@example.com")
+    page.current_url.should == "http://test.example.com/subscribem/"
   end
 end
